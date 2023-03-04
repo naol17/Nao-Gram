a1c12e4aec4ac32036e20f4fc630ea4a08567e57
naol17 <it.naol.dame12@gmail.com>
2023-03-04 18:06:15 +0300

    change he main page

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+     
+      <div className='bg-slate-500'>
+        <div className='text-red-500 font-mono border rounded'>jalqabe</div>
+        
+      </div>
+    </>
+  )
+}
