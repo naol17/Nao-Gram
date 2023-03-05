d37fee9f65b33c4fd791bccada6d15d577fc6c83
naol17 <it.naol.dame12@gmail.com>
2023-03-06 00:07:07 +0300

    update app

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,13 +1,16 @@
+import nextAuth from 'next-auth'
 import Head from 'next/head'
-
+import {signIn,useSession}from "next-auth/react"
 export default function Home() {
+  const {data}= useSession();
+  console.log('dataaa', data)
   return (
     <>
      
-      <div className='bg-slate-500'>
-        <div className='text-red-500 font-mono border rounded'>jalqabe</div>
-        
-      </div>
+      <div className='bg-slate-500'>     
+      
+      <button onClick={()=>signIn("google")}>sign in</button>
+       </div>
     </>
   )
 }
