dd0f8182f49da82d7474d21e3d083ac57095b062
naol17 <it.naol.dame12@gmail.com>
2023-03-06 15:59:32 +0300

    update auth

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,24 +1,24 @@
 import nextAuth from 'next-auth'
 import Head from 'next/head'
-import {signIn,signOut,useSession}from "next-auth/react"
+import { signIn, signOut, useSession } from "next-auth/react"
 export default function Home() {
-  const {data}= useSession();
+  const { data } = useSession();
   console.log('dataaa', data)
   return (
     <>
-     
-      <div className='bg-slate-500'>     
-      {
-        data?.user?(<button onClick={()=> signOut()}>sign out</button>
-        ): 
-(                <button onClick={()=>signIn("google")}>sign in</button>
 
-)      
-      }
-      {
-       data?.user?.name
-      }
-       </div>
+      <div className='bg-slate-500'>
+        {
+          data?.user ? (<button onClick={() => signOut()}>sign out</button>
+          ) :
+            (<button onClick={() => signIn("google")}>sign in</button>
+
+            )
+        }
+        {
+          data?.user?.name
+        }
+      </div>
     </>
   )
 }
